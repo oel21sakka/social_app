833c79ec8719f508749174ffab2c2540d9e70457
oel21sakka <oel21sakka@gmail.com>
2023-12-02 21:06:46 +0200

    add email authentication to allow users login by email

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from account.forms import UserRegistrationForm,UserEditForm,ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 @login_required
 def dashboard(request):
@@ -15,7 +16,10 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            messages.success(request,'Account Created Successfully')
             return render(request,'account/register_done.html',{'new_user':new_user})
+        else:
+            messages.error(request,'Error Creating Your Account')
     else:
         user_form = UserRegistrationForm()
     return render(request,'account/register.html',{'user_form':user_form})
@@ -28,7 +32,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-        
+            messages.success(request,'Profile Updated Successfully')
+        else:
+            messages.error(request,'Error Updating Your Profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
--- social_app_django/settings.py
+++ social_app_django/settings.py
@@ -100,6 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Authentication backends
+
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+'account.authentication.EmailAuthBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
