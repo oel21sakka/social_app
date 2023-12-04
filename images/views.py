54f9c8338e05feaf22131a021ce3122a96274971
oel21sakka <oel21sakka@gmail.com>
2023-12-04 22:41:35 +0200

    add image detail view

--- images/views.py
+++ images/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from .forms import ImageCreateForm
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -12,7 +15,29 @@ def image_create(request):
             new_image.user = request.user
             new_image.save()
             messages.success(request,'Image Added Successfully')
-            return redirect('dashboard')
+            return redirect(new_image.get_absolute_url())
     else:
         form = ImageCreateForm(data=request.GET)
     return render(request,'images/image/create.html',{'section':'images','form':form})
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,'images/image/detail.html',{'section':'images','image':image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = get_object_or_404(Image,id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status':'error'})
--- social_app_django/settings.py
+++ social_app_django/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'images',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
