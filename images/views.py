8f6912bfcd39b088b0b06939eb0b959ea40d1ba2
oel21sakka <oel21sakka@gmail.com>
2023-12-04 14:48:19 +0200

    add images app

---
+++ images/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
+
+@login_required
+def image_create(request):
+    if request.method=='POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request,'Image Added Successfully')
+            return redirect('dashboard')
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(request,'images/image/create.html',{'section':'images','form':form})
